3ba0d7415e9ba57f2c354f3da46df81801260cad
jawid-fakhari <jawid_fakhari@live.it>
2024-10-14 15:45:55 +0200

    .gitignore

---
+++ src/Experience.jsx
@@ -0,0 +1,16 @@
+import { OrbitControls } from '@react-three/drei'
+import { Perf } from 'r3f-perf'
+
+export default function Experience() {
+
+
+    return <>
+
+        <Perf position="top-left" />
+
+        <OrbitControls makeDefault />
+
+
+
+    </>
+}
