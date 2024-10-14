6c6e4010fc560049b85fcddb7f11ddb095259bab
jawid-fakhari <jawid_fakhari@live.it>
2024-10-14 17:34:51 +0200

    Optimisation better way

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,15 +1,26 @@
 import { Center, OrbitControls, Text3D, useMatcapTexture } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
+// import { useEffect, useState } from 'react'
+import * as THREE from 'three'
+
+const torusGeometry = new THREE.TorusGeometry()
+const material = new THREE.MeshMatcapMaterial()
 
 export default function Experience() {
-    const [torusGeometry, setTorusGeometry] = useState()
-    const [material, setMaterial] = useState()
+    // const [torusGeometry, setTorusGeometry] = useState()
+    // const [material, setMaterial] = useState()
 
     //questo matcapTexture viene da una libreria di texture di github "7B5254_E9DCC7_B19986_C8AC91"
     const [matcapTexture] = useMatcapTexture('7B5254_E9DCC7_B19986_C8AC91', 256)
 
+    useEffect(() => {
+        matcapTexture.colorSpace = THREE.SRGBColorSpace
+        matcapTexture.needsUpdate = true
 
+        material.matcap = matcapTexture
+        material.needsUpdate = true
+    }, [])
 
     const tempArray =
         [...Array(100)]
@@ -31,15 +42,16 @@ export default function Experience() {
                 >
                 </mesh>
             )
+
     return <>
 
         <Perf position="top-left" />
 
         <OrbitControls makeDefault />
 
-        <torusGeometry ref={setTorusGeometry} />
+        {/* <torusGeometry ref={setTorusGeometry} />
 
-        <meshMatcapMaterial ref={setMaterial} matcap={matcapTexture} />
+        <meshMatcapMaterial ref={setMaterial} matcap={matcapTexture} /> */}
 
         {/* Center helper mette il text al centro */}
         <Center>
